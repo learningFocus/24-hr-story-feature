6ac81abbe87033fe391117f7fbddc694bd8d15e9
learningFocus <manishtamang20012024@gmail.com>
2026-01-18 14:32:40 +0545

    initial commit from new machine

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  console.log("recreating a feature from instagram");
+  return (
+    <main className="p-10">
+      <h1 className="text-blue-500 text-2xl text-center">
+        Instagram Story Features
+      </h1>
+    </main>
+  );
+}
